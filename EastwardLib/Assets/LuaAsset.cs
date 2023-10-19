31c389a076c5624107ac5f4c4f2366492db6cc85
vriskaserket51 <serketvriska51@gmail.com>
2023-10-19 21:07:26 +0900

    Compiled Lua asset type

---
+++ EastwardLib/Assets/LuaAsset.cs
@@ -0,0 +1,19 @@
+﻿namespace EastwardLib.Assets;
+
+// TODO implement this
+public class LuaAsset : Asset
+{
+    public LuaAsset()
+    {
+    }
+
+    public override byte[] Encode()
+    {
+        return base.Encode();
+    }
+
+    public override void Decode(Stream s)
+    {
+        base.Decode(s);
+    }
+}
